a5ca9d30a4f1891a789cf51231b334aee8aeaeda
mkraja826 <37881042+mkraja826@users.noreply.github.com>
2026-08-16 05:56:31 +0530

    feat: expose MiCirql design engine catalogs

---
+++ packages/design-engine/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./website-archetypes";
+export * from "./component-taxonomy";
+export * from "./core-structural-blueprints";
+export * from "./content-structural-blueprints";
+export * from "./palette-strategies";
+export * from "./archetype-blueprint-rules";

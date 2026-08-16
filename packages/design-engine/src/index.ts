d8e6e3d33625b7e9786491237abfcb4fb0b99f3f
mkraja826 <37881042+mkraja826@users.noreply.github.com>
2026-08-16 07:10:47 +0530

    feat: export industry fit helpers

--- packages/design-engine/src/index.ts
+++ packages/design-engine/src/index.ts
@@ -11,3 +11,4 @@ export * from "./content-quality";
 export * from "./design-diversity";
 export * from "./preference-learning";
 export * from "./industry-intelligence";
+export * from "./industry-fit";

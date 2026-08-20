9ca861c086cf06fb0b4d238d642c1758b4984e85
mkraja826 <37881042+mkraja826@users.noreply.github.com>
2026-08-20 07:00:32 +0530

    Export gallery lightbox runtime

--- packages/sections/src/index.ts
+++ packages/sections/src/index.ts
@@ -3,3 +3,4 @@ export * from "./premium-certification";
 export * from "./sections";
 export { Section as SeedSection } from "./sections";
 export * from "./registry";
+export * from "./gallery-lightbox-runtime";
